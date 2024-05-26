7fe83a98c6d654bbb84ed37ed5d0bc9b2cde0a11
Ankit <ankit001nep@gmail.com>
2024-05-26 15:42:00 +0545

    Hero Section added with animation (Hero.tsx)

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,11 @@
+
+const Hero = () => {
+    return (
+        <div className="p-14 ">
+            <h1 className="text-lg md:text-xl lg:text-3xl font-bold ">This is Inter Project.</h1>
+            <p className="text-justify font-serif ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error eos animi quasi harum sed accusantium deleniti minus ad, neque vel id nam quis est cum itaque rem soluta quo commodi facilis. Tempora fuga unde obcaecati a vel numquam id, saepe facilis enim! Architecto delectus nisi tempora voluptas, odio expedita ipsam culpa deserunt cumque? Modi inventore, voluptatem provident, assumenda expedita libero maxime alias amet, exercitationem aliquid consequatur maiores sapiente similique odio corporis atque id recusandae dolor non quod at in ea quos? Dolorum itaque sunt commodi dolor odit consequuntur ducimus aliquam, architecto quisquam repellat? Dicta quis vel iste eligendi itaque nemo.</p>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/components/home/HeroAnimation.tsx
@@ -0,0 +1,19 @@
+import './Animation.css'
+const HeroAnimation = () => {
+    return (
+        <div className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </div>
+    )
+}
+
+export default HeroAnimation
