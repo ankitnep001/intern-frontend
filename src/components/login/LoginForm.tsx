655c6a0361e5f51ceb88ecc0e0aeda500eb0e348
Ankit <ankit001nep@gmail.com>
2024-05-27 15:28:45 +0545

    password seen and hidden toggle

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,9 +1,58 @@
-
+import { useState } from "react";
+import { IoIosEyeOff, IoMdEye } from "react-icons/io";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { instance } from "services/instance";
+
 const LoginForm = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [_getData, setGetData] = useState<any>([]);
+
+    //handle email change
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+    };
+
+    //handle password change
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
+
+    //handle Submit
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log("email:", email);
+        console.log("password:", password);
+
+        setEmail("");
+        setPassword("");
+    }
+
+    const handlePasswordToggle = () => {
+        setShowPassword(!showPassword)
+    }
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        instance({
+            method: 'post',
+            url: '/auth',
+            data: {
+                username: email,
+                password: password
+            }
+        }).then((response) => {
+            setGetData(response.data);
+            console.log(response)
+        })
+            .catch(error => console.log(error));
+    }
+
     return (
-        <form action="" className="flex justify-center items-center h-[86vh]">
+        <form onSubmit={handleSubmit} className="flex justify-center items-center h-[86vh]">
             <div className="flex flex-col bg-[#fefeff] shadow-md rounded-lg p-6 w-full max-w-md">
                 <h2 className="text-center text-2xl font-bold mb-4">Login</h2>
 
@@ -16,6 +65,8 @@ const LoginForm = () => {
                         <input
                             type="email"
                             id="email"
+                            value={email}
+                            onChange={handleEmailChange}
                             placeholder="Enter your email"
                             className="w-full  pl-10 pr-3 py-2 border   rounded-md focus:outline-none focus:ring focus:border-blue-500"
                             required
@@ -30,17 +81,24 @@ const LoginForm = () => {
                     <div className="relative flex items-center">
                         <RiLockPasswordLine className="absolute left-3 text-gray-500" />
                         <input
-                            type="password"
+                            type={showPassword ? "text" : "password"}
                             id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            autoComplete="on"
                             placeholder="Enter your Password"
                             className="w-full  pl-10 pr-3 py-2 border   rounded-md focus:outline-none focus:ring focus:border-blue-500"
                             required
                         />
+                        <div className=" absolute right-3 text-gray-500 cursor-pointer" onClick={handlePasswordToggle}>
+                            {showPassword ? <IoMdEye /> : <IoIosEyeOff />}
+                        </div>
                     </div>
                 </div>
 
                 <button
                     type="submit"
+                    onClick={handleClick}
                     className="w-full bg-blue-400 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
                 >
                     Login
