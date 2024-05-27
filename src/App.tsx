4eea8274e81b96491b77c4fa009713b1cb6218bf
Ankit <ankit001nep@gmail.com>
2024-05-27 15:32:05 +0545

    added Outlet (Template.tsx && App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import Header from "@components/header/Header"
 import About from "@pages/About"
 import Home from "@pages/Home"
 import Login from "@pages/Login"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Templates from "templates/Templates"
+
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Templates />,
+  children: [
+    { index: true, element: <Home /> },
+    { path: '/about', element: <About /> },
+    { path: '/login', element: <Login /> }
+  ]
+}])
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/templates/Templates.tsx
@@ -0,0 +1,16 @@
+import Footer from '@components/footer/Footer'
+import Header from '@components/header/Header'
+import { Outlet } from 'react-router-dom'
+
+const Templates = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+
+        </div>
+    )
+}
+
+export default Templates
