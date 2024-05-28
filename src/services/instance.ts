7db525452dda4a3ab50ee0be8902c2980e1cd8f4
Ankit <ankit001nep@gmail.com>
2024-05-28 15:30:01 +0545

    global interface and axios instance (gloal.interface.ts & instance.ts

---
+++ src/services/instance.ts
@@ -0,0 +1,6 @@
+import axios, { AxiosInstance } from "axios";
+
+export const axiosInstance: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_APP_BASE_URL,
+    timeout: import.meta.env.TIME_OUT
+});
