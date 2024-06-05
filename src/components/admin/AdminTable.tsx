5e49c57a397ca6625cb1234418d0c64807701019
Ankit <ankit001nep@gmail.com>
2024-06-05 11:11:19 +0545

    featureAdded:Sort by name (AdminTable.tsx)

--- src/components/admin/AdminTable.tsx
+++ src/components/admin/AdminTable.tsx
@@ -2,8 +2,9 @@ import { toast } from "@components/toast/ToastManages";
 import { GetAdminListProps } from "@interface/global.interface";
 import axiosInstance from "@services/instance";
 import { useEffect, useState } from "react";
-// import { FaUsersViewfinder } from "react-icons/fa6";
-import { MdDeleteOutline } from "react-icons/md";
+import { MdArrowDropDown, MdArrowDropUp, MdDeleteOutline } from "react-icons/md";
+import { RiExpandUpDownFill } from "react-icons/ri";
+
 import EditAdmin from "./EditAdmin";
 import ViewDetails from "./ViewDetails";
 
@@ -12,14 +13,14 @@ const AdminTable = () => {
     const [modal, setModal] = useState<boolean>(false);
     const [modalContent, setModalContent] = useState<'view' | 'edit'>('view');
     const [selectedAdminId, setSelectedAdminId] = useState<string | null>(null);
+    const [sortStatus, setSortStatus] = useState<0 | 1 | 2>(0); // 0: both, 1: down, 2: up
+
 
     useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const response = await axiosInstance.get('/admin');
-
-                // console.log(response.data.data?.data);
-                setAdminList(response.data.data?.data);
+                setAdminList(response.data.data?.data || []);
             } catch (error) {
                 console.error('Error fetching users:', error);
             }
@@ -28,38 +29,67 @@ const AdminTable = () => {
         fetchUsers();
     }, []);
 
-    // delete admin
+
+
     const handleDelete = async (id: string) => {
         try {
             await axiosInstance.delete(`/admin/${id}`);
             setAdminList(prevList => prevList.filter(admin => admin.id !== id));
             toast.show({ title: "Success", content: "Deleted successfully", duration: 2000, type: 'success' });
         } catch (error) {
-            console.error('Error fetching users:', error);
-            toast.show({ title: "Error", content: "Created successfully", duration: 2000, type: 'error' });
+            console.error('Error deleting admin:', error);
+            toast.show({ title: "Error", content: "Delete unsuccessful", duration: 2000, type: 'error' });
         }
-
     };
 
     const openModal = (type: 'view' | 'edit', adminId: string) => {
         setModalContent(type);
         setSelectedAdminId(adminId);
-        setModal(prevState => !prevState);
+        setModal(true);
     };
 
     const closeModal = () => {
         setModal(false);
         setSelectedAdminId(null);
     };
+
+    //sort
+    const handleSortClick = () => {
+        setSortStatus(prevStatus => {
+            const newStatus = prevStatus === 0 ? 1 : prevStatus === 1 ? 2 : 0;
+
+            const sortedList = [...adminList].sort((a, b) => {
+                const firstNameA = a.details.firstName?.en.toLowerCase() || '';
+                const firstNameB = b.details.firstName?.en.toLowerCase() || '';
+
+                if (newStatus === 1) {
+                    return firstNameB.localeCompare(firstNameA); // Descending order
+                } else if (newStatus === 2) {
+                    return firstNameA.localeCompare(firstNameB); // Ascending order
+                }
+                return 0; // Default, no sorting
+            });
+
+            setAdminList(sortedList);
+            return newStatus;
+        });
+    };
+
     return (
-        <div className=" table-container relative overflow-x-auto w-full mt-10 px-5 ">
-            <table className="w-full text-sm text-center  ">
-                <thead className=" uppercase bg-blue-400 ">
-                    <tr className="">
+        <div className="table-container relative overflow-x-auto w-full mt-10 px-5">
+            <table className="w-full text-sm text-center">
+                <thead className="uppercase bg-blue-400">
+                    <tr>
                         <th className="px-6 py-3">S.N</th>
-                        <th className="px-6 py-3">Username</th>
-                        <th className="px-6 py-3">FirstName</th>
+                        <th className="px-6 py-3 flex justify-center items-center">FirstName
+                            <div className="p-2 cursor-pointer" onClick={handleSortClick}>
+                                {sortStatus === 0 && <RiExpandUpDownFill size={22} />}
+                                {sortStatus === 1 && <MdArrowDropDown size={24} />}
+                                {sortStatus === 2 && <MdArrowDropUp size={24} />}
+                            </div>
+                        </th>
                         <th className="px-6 py-3">LastName</th>
+                        <th className="px-6 py-3">Username</th>
                         <th className="px-6 py-3">Email</th>
                         <th className="px-6 py-3">Role</th>
                         <th className="px-6 py-3">View</th>
@@ -68,15 +98,15 @@ const AdminTable = () => {
                     </tr>
                 </thead>
                 <tbody className="w-full">
-                    {adminList?.length > 0 && adminList.map((item, index) => {
-                        return (
-                            <tr key={index} className="odd:bg-white even:bg-blue-300 ">
+                    {adminList?.length > 0 && adminList.map((item, index) => (
+                        item && ( // Ensure the item is not undefined
+                            <tr key={item.id} className="odd:bg-white even:bg-blue-300">
                                 <td className="px-6 py-3">{index + 1}</td>
-                                <td className="px-6 py-3">{item?.username}</td>
                                 <td className="px-6 py-3">{item.details.firstName?.en}</td>
                                 <td className="px-6 py-3">{item.details.lastName?.en}</td>
-                                <td className="px-6 py-3">{item?.email}</td>
-                                <td className="px-6 py-3">{item?.role.toString().toLowerCase()}</td>
+                                <td className="px-6 py-3">{item.username}</td>
+                                <td className="px-6 py-3">{item.email}</td>
+                                <td className="px-6 py-3">{item.role.toString().toLowerCase()}</td>
                                 <td className="px-6 py-3 font-bold text-blue-700 cursor-pointer underline" onClick={() => openModal('view', item.id)}>
                                     View
                                 </td>
@@ -84,12 +114,11 @@ const AdminTable = () => {
                                     Edit
                                 </td>
                                 <td className="px-6 py-3 text-red-600 inline-block cursor-pointer">
-                                    <MdDeleteOutline size={24} onClick={() => handleDelete(item?.id)} />
+                                    <MdDeleteOutline size={24} onClick={() => handleDelete(item.id)} />
                                 </td>
                             </tr>
-                        );
-                    })}
-
+                        )
+                    ))}
                 </tbody>
             </table>
             {modal && (
@@ -98,13 +127,16 @@ const AdminTable = () => {
                         <div className="flex justify-end">
                             <button onClick={closeModal} className="text-red-500 hover:bg-red-600 hover:text-white p-2 rounded-lg">Close</button>
                         </div>
-                        {modalContent === 'view' ? <ViewDetails adminId={selectedAdminId} /> : <EditAdmin adminId={selectedAdminId} onClose={closeModal} />}
+                        {modalContent === 'view' ? (
+                            <ViewDetails adminId={selectedAdminId} />
+                        ) : (
+                            <EditAdmin adminId={selectedAdminId} onClose={closeModal} />
+                        )}
                     </div>
                 </div>
             )}
         </div>
+    );
+};
 
-    )
-}
-
-export default AdminTable
+export default AdminTable;
