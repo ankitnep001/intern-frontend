8add4a64b9e2b5c8563d7d1a1fca26e20384d236
Ankit <ankit001nep@gmail.com>
2024-06-13 14:25:32 +0545

    open on pc message from medium screen

--- src/templates/AdminTemplate.tsx
+++ src/templates/AdminTemplate.tsx
@@ -2,16 +2,18 @@ import AdminSidebar from "@components/admin/AdminSidebar";
 import { Outlet } from "react-router-dom";
 
 const AdminTemplate = () => {
-
     return (
-        <>
-            {/* <div className=""> */}
+        <div className="flex h-screen">
             <AdminSidebar />
-            <div className="w-full h-full pl-44 md:pl-72">
-                <Outlet />
+            <div className="w-full h-full pl-44 md:pl-64 flex justify-center items-center">
+                <div className="block md:hidden">
+                    <p className="text-lg font-bold">Please open this page on a PC or laptop for the best experience.</p>
+                </div>
+                <div className="hidden md:block w-full h-full ">
+                    <Outlet />
+                </div>
             </div>
-            {/* </div> */}
-        </>
+        </div>
     );
 };
 
